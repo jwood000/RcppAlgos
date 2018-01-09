263fca24f15770ba4579d72bcc758efb5376026f
Joseph Brant Wood <josephwood@Josephs-MacBook-Pro.local>
2018-01-09 16:14:01 -0500

    Headers for pollard's rho factorization

---
+++ inst/include/PollardRho.h
@@ -0,0 +1,13 @@
+#ifndef POLLARDRHO_R
+#define POLLARDRHO_R 1
+
+/**
+ * Get Prime Factorization
+ * t: number to factorize
+ * factors [out]: the list of factors
+ *
+ * Note: this is adapted from demo "factorize.c" file from gmplib
+ */
+void getPrimefactors (double& t, std::vector<double>&  factors);
+
+#endif
