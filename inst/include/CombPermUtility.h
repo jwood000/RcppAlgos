3bd72dc9f3f402042c6956b4822cc8c1f2e0314d
Joseph Brant Wood <jwood000@gmail.com>
2018-05-12 04:13:27 -0400

    Added more functions to utility file

--- inst/include/CombPermUtility.h
+++ inst/include/CombPermUtility.h
@@ -1,12 +1,18 @@
 #ifndef RcppAlgos_CombPermUtility_h
 #define RcppAlgos_CombPermUtility_h
 
+#include <Rcpp.h>
+
+typedef double (*funcPtr)(std::vector<double>& x);
+typedef bool (*compPtr)(double& x, double& y);
+
 double NumPermsWithRep(std::vector<int> v);
 double NumPermsNoRep(int n, int k);
 double nChooseK(double n, double k);
-double GetRowNum(int n, int r);
+double NumCombsWithRep(int n, int r);
 double MultisetCombRowNum(int n, int r, std::vector<int> Reps);
-Rcpp::IntegerMatrix MakeIndexHeaps(unsigned long int indRows, unsigned long int r);
+double MultisetPermRowNum(int n, int r, std::vector<int> Reps, 
+                          Rcpp::IntegerMatrix myCombs);
 
 // Contraint functions
 double prodCpp(std::vector<double>& v);
@@ -14,5 +20,14 @@ double sumCpp(std::vector<double>& v);
 double meanCpp(std::vector<double>& v);
 double maxCpp(std::vector<double>& v);
 double minCpp(std::vector<double>& v);
+
+bool lessCpp(double& x, double& y);
+bool greaterCpp(double& x, double& y);
+bool lessEqualCpp(double& x, double& y);
+bool greaterEqualCpp(double& x, double& y);
+bool equalCpp(double& x, double& y);
+
+Rcpp::XPtr<funcPtr> putFunPtrInXPtr(std::string fstr);
+Rcpp::XPtr<compPtr> putCompPtrInXPtr(std::string fstr);
 
 #endif
--- src/CombPermUtility.cpp
+++ src/CombPermUtility.cpp
@@ -5,6 +5,7 @@
 // from Hadley Wickham's article titled 
 // "High Performance functions with Rcpp"
 // found: http://adv-r.had.co.nz/Rcpp.html
+
 Rcpp::List rleCpp(std::vector<int> x) {
     std::vector<unsigned long int> lengths, numUni;
     std::vector<int> values;
@@ -42,17 +43,13 @@ double NumPermsWithRep(std::vector<int> v) {
     unsigned long int i, j, numUni = myUnis[0];
     double result = 1;
     
-    for (i = n; i > myMax; i--) {result *= i;}
+    for (i = n; i > myMax; i--)
+        result *= i;
     
-    if (numUni > 0) {
-        for (i = 1; i <= numUni; i++) {
-            // No need to divide by 1.
-            // Start j at 2 instead.
-            for (j = 2; j <= myLens[i]; j++) {
+    if (numUni > 0)
+        for (i = 1; i <= numUni; i++)
+            for (j = 2; j <= myLens[i]; j++)
                 result /= j;
-            }
-        }
-    }
     
     return result;
 }
@@ -65,42 +62,54 @@ double NumPermsNoRep(int n, int k) {
 }
 
 double nChooseK(double n, double k) {
-    // returns the number of k-combinations from a set
-    // of n elements. Mathematically speaking, 
-    //  we have: n!/(k!*(n-k)!)
+    // Returns number of k-combinations from n elements.
+    // Mathematically speaking, we have: n!/(k!*(n-k)!)
+    
+    if (k == n || k == 0)
+        return 1;
+    
     double nCk;
     double temp = 1;
-    for(int i = 1; i <= k; i++) {temp *= (n - k + i)/i;}
+    for(int i = 1; i <= k; i++)
+        temp *= (n - k + i)/i;
+    
     nCk = round(temp);
     return nCk;
 }
 
-double GetRowNum(int n, int r) {
-    // for combinations where repetition is allowed, this
+double NumCombsWithRep(int n, int r) {
+    // For combinations where repetition is allowed, this
     // function returns the number of combinations for
     // a given n and r. The resulting vector, "triangleVec"
     // resembles triangle numbers. In fact, this vector
     // is obtained in a very similar method as generating
     // triangle numbers, albeit in a repeating fashion.
+    
+    if (r == 0)
+        return 1;
+    
     int i, k;
     std::vector<double> triangleVec(n);
     std::vector<double> temp(n);
-    for (i = 0; i < n; i++) {triangleVec[i] = i+1;}
+    
+    for (i = 0; i < n; i++)
+        triangleVec[i] = i+1;
     
     for (i = 1; i < r; i++) {
-        for (k = 1; k <= n; k++) {
+        for (k = 1; k <= n; k++)
             temp[k-1] = std::accumulate(triangleVec.begin(), triangleVec.begin() + k, 0.0);
-        }
+
         triangleVec = temp;
     }
     
     return triangleVec[n-1];
 }
 
-// Slightly different than GetRowNum above as we can't
+// Slightly different than CombsWithRep above as we can't
 // guarantee 1) the repetition of each element is
 // greater than or equal to n, and 2) that the
 // repetition of the each element isn't the same
+
 double MultisetCombRowNum(int n, int r, std::vector<int> Reps) {
     int i, k, j, myMax, r1 = r+1;
     std::vector<double> triangleVec(r1);
@@ -116,10 +125,13 @@ double MultisetCombRowNum(int n, int r, std::vector<int> Reps) {
     for (k = 1; k < n; k++) {
         for (i = r; i > 0; i--) {
             myMax = i - Reps[k];
-            if (myMax < 0) {myMax = 0;}
+            if (myMax < 0)
+                myMax = 0;
             
             tempSum = 0;
-            for (j = myMax; j <= i; j++) {tempSum += triangleVec[j];}
+            for (j = myMax; j <= i; j++)
+                tempSum += triangleVec[j];
+            
             temp[i] = tempSum;
         }
         triangleVec = temp;
@@ -128,33 +140,42 @@ double MultisetCombRowNum(int n, int r, std::vector<int> Reps) {
     return triangleVec[r];
 }
 
-
-Rcpp::IntegerMatrix MakeIndexHeaps(unsigned long int indRows, unsigned long int r) {
-    unsigned long int j, i = 0, count = 0;
-    Rcpp::IntegerMatrix indexMatrix(indRows, r);
+double MultisetPermRowNum(int n, int r, 
+                          std::vector<int> Reps, 
+                          Rcpp::IntegerMatrix myCombs) {
     
-    std::vector<unsigned long int> vecInd(r, 0);
-    Rcpp::IntegerVector mySeq = Rcpp::seq(0, r-1);
-    for (j = 0; j < r; j++) {indexMatrix(count, j) = j;}
+    int combRows = (int) MultisetCombRowNum(n, r, Reps);
+    std::vector<int> rowVec(r);
+    Rcpp::IntegerVector temp(1);
+    bool KeepGoing = true;
+    double numRows = 0, computedRows;
+    int rowCount;
     
-    while (i < r) {
-        if (vecInd[i] < i) {
-            if (i % 2 == 0)
-                std::swap(mySeq[0], mySeq[i]);
-            else
-                std::swap(mySeq[vecInd[i]], mySeq[i]);
-            count++;
-            for (j = 0; j < r; j++) {indexMatrix(count,j) = mySeq[j];}
-            vecInd[i]++;
-            i = 0;
-        } else {
-            vecInd[i] = 0;
-            i++;
+    for (std::size_t i = 0; i < combRows; i++) {
+        int j = 0, k = 0;
+        while (j < r) {
+            rowVec[j] = k;
+            temp[0] = myCombs(i, j);
+            KeepGoing = true;
+            while (KeepGoing) {
+                rowVec[j] = k;
+                j++;
+                if (j >= r)
+                    KeepGoing = false;
+                else if (myCombs(i, j) != temp[0])
+                    KeepGoing = false;
+            }
+            k++;
         }
+
+        computedRows = NumPermsWithRep(rowVec);
+        rowCount = (int) computedRows;
+        numRows += rowCount;
     }
     
-    return indexMatrix;
+    return numRows;
 }
+
 
 // Below, we define five functions that will be utilized
 // as constraint functions. We also define five comparison
@@ -190,4 +211,41 @@ double maxCpp(std::vector<double>& v) {
 double minCpp(std::vector<double>& v) {
     std::vector<double>::iterator y = std::min_element(v.begin(), v.end());
     return v[std::distance(v.begin(), y)];
+}
+
+// Comparison functions
+bool lessCpp(double& x, double& y) {return x < y;}
+bool greaterCpp(double& x, double& y) {return x > y;}
+bool lessEqualCpp(double& x, double& y) {return x <= y;}
+bool greaterEqualCpp(double& x, double& y) {return x >= y;}
+bool equalCpp(double& x, double& y) {return x == y;}
+
+Rcpp::XPtr<funcPtr> putFunPtrInXPtr(std::string fstr) {
+    if (fstr == "prod")
+        return(Rcpp::XPtr<funcPtr>(new funcPtr(&prodCpp)));
+    else if (fstr == "sum")
+        return(Rcpp::XPtr<funcPtr>(new funcPtr(&sumCpp)));
+    else if (fstr == "mean")
+        return(Rcpp::XPtr<funcPtr>(new funcPtr(&meanCpp)));
+    else if (fstr == "max")
+        return(Rcpp::XPtr<funcPtr>(new funcPtr(&maxCpp)));
+    else if (fstr == "min")
+        return(Rcpp::XPtr<funcPtr>(new funcPtr(&minCpp)));
+    else
+        return Rcpp::XPtr<funcPtr>(R_NilValue); // runtime error as NULL no XPtr
+}
+
+Rcpp::XPtr<compPtr> putCompPtrInXPtr(std::string fstr) {
+    if (fstr == "<")
+        return(Rcpp::XPtr<compPtr>(new compPtr(&lessCpp)));
+    else if (fstr == ">")
+        return(Rcpp::XPtr<compPtr>(new compPtr(&greaterCpp)));
+    else if (fstr == "<=")
+        return(Rcpp::XPtr<compPtr>(new compPtr(&lessEqualCpp)));
+    else if (fstr == ">=")
+        return(Rcpp::XPtr<compPtr>(new compPtr(&greaterEqualCpp)));
+    else if (fstr == "==")
+        return(Rcpp::XPtr<compPtr>(new compPtr(&equalCpp)));
+    else
+        return Rcpp::XPtr<compPtr>(R_NilValue); // runtime error as NULL no XPtr
 }
