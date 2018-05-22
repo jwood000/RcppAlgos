a4af6d52f9ae7555470b9a8370e35bb045501d23
Joseph Brant Wood <jwood000@gmail.com>
2018-05-21 20:07:40 -0400

    Changed file name and removed constraint functions

---
+++ inst/include/CombPermUtils.h
@@ -0,0 +1,19 @@
+#ifndef RcppAlgos_CombPermUtils_h
+#define RcppAlgos_CombPermUtils_h
+
+#include <Rcpp.h>
+
+double NumPermsWithRep(std::vector<int> v);
+double NumPermsNoRep(int n, int k);
+double nChooseK(double n, double k);
+double NumCombsWithRep(int n, int r);
+double MultisetCombRowNum(int n, int r, std::vector<int> Reps);
+double MultisetPermRowNum(int n, int r, std::vector<int> myReps);
+
+void nextFullPerm(uint16_t *myArray, unsigned long int n1);
+
+void nextPartialPerm(uint16_t *myArray, unsigned long int nCols, 
+                     unsigned long int r1, unsigned long int r,
+                     unsigned long int n1, unsigned long int n);
+
+#endif
