1a0353e3ecbb6c1bbd9647734614d2ffa90906b8
Joseph Brant Wood <jwood000@gmail.com>
2018-06-03 13:11:53 -0400

    Added compound comparisons

--- inst/include/ConstraintsUtils.h
+++ inst/include/ConstraintsUtils.h
@@ -1,21 +1,31 @@
 #ifndef CONSTRAINTS_UTILS_H
 #define CONSTRAINTS_UTILS_H
 
-typedef double (*funcPtr)(std::vector<double>& x);
-typedef bool (*compPtr)(double& x, double& y);
+typedef double (*funcPtr)(std::vector<double> &x);
+typedef bool (*compPtr)(double &x, std::vector<double> &y);
+
+const std::vector<std::string> compSpecial = {"==", ">,<", ">=,<", ">,<=", ">=,<="};
+const std::vector<std::string> compHelper = {"<=", "<", "<", "<=", "<="};
 
 // Constraint functions
-double prodCpp(std::vector<double>& v);
-double sumCpp(std::vector<double>& v);
-double meanCpp(std::vector<double>& v);
-double maxCpp(std::vector<double>& v);
-double minCpp(std::vector<double>& v);
+double prodCpp(std::vector<double> &v);
+double sumCpp(std::vector<double> &v);
+double meanCpp(std::vector<double> &v);
+double maxCpp(std::vector<double> &v);
+double minCpp(std::vector<double> &v);
 
-bool lessCpp(double& x, double& y);
-bool greaterCpp(double& x, double& y);
-bool lessEqualCpp(double& x, double& y);
-bool greaterEqualCpp(double& x, double& y);
-bool equalCpp(double& x, double& y);
+// Standard comparison functions
+bool lessCpp(double &x, std::vector<double> &y);
+bool greaterCpp(double &x, std::vector<double> &y);
+bool lessEqualCpp(double &x, std::vector<double> &y);
+bool greaterEqualCpp(double &x, std::vector<double> &y);
+bool equalCpp(double &x, std::vector<double> &y);
+
+// In between comparisons
+bool greaterLessCpp(double &x, std::vector<double> &y);
+bool greaterEqlLessCpp(double &x, std::vector<double> &y);
+bool greaterLessEqlCpp(double &x, std::vector<double> &y);
+bool greaterEqlLessEqlCpp(double &x, std::vector<double> &y);
 
 Rcpp::XPtr<funcPtr> putFunPtrInXPtr(std::string fstr);
 Rcpp::XPtr<compPtr> putCompPtrInXPtr(std::string fstr);
--- src/ConstraintsUtils.cpp
+++ src/ConstraintsUtils.cpp
@@ -1,7 +1,7 @@
 #include <Rcpp.h>
 
-typedef double (*funcPtr)(std::vector<double>& x);
-typedef bool (*compPtr)(double& x, double& y);
+typedef double (*funcPtr)(std::vector<double> &x);
+typedef bool (*compPtr)(double &x, std::vector<double> &y);
 
 // Below, we define five functions that will be utilized
 // as constraint functions. We also define five comparison
@@ -39,12 +39,18 @@ double minCpp(std::vector<double>& v) {
     return v[std::distance(v.begin(), y)];
 }
 
-// Comparison functions
-bool lessCpp(double& x, double& y) {return x < y;}
-bool greaterCpp(double& x, double& y) {return x > y;}
-bool lessEqualCpp(double& x, double& y) {return x <= y;}
-bool greaterEqualCpp(double& x, double& y) {return x >= y;}
-bool equalCpp(double& x, double& y) {return x == y;}
+// Standard comparison functions
+bool lessCpp(double &x, std::vector<double> &y) {return x < y[0];}
+bool greaterCpp(double &x, std::vector<double> &y) {return x > y[0];}
+bool lessEqualCpp(double &x, std::vector<double> &y) {return x <= y[0];}
+bool greaterEqualCpp(double &x, std::vector<double> &y) {return x >= y[0];}
+bool equalCpp(double &x, std::vector<double> &y) {return x == y[0];}
+
+// Compound comparison functions for finding values between a given range
+bool greaterLessCpp(double &x, std::vector<double> &y) {return x < y[0] && x > y[1];}
+bool greaterEqlLessCpp(double &x, std::vector<double> &y) {return x < y[0] && x >= y[1];}
+bool greaterLessEqlCpp(double &x, std::vector<double> &y) {return x <= y[0] && x > y[1];}
+bool greaterEqlLessEqlCpp(double &x, std::vector<double> &y) {return x <= y[0] && x >= y[1];}
 
 Rcpp::XPtr<funcPtr> putFunPtrInXPtr(std::string fstr) {
     if (fstr == "prod")
@@ -61,17 +67,46 @@ Rcpp::XPtr<funcPtr> putFunPtrInXPtr(std::string fstr) {
         return Rcpp::XPtr<funcPtr>(R_NilValue); // runtime error as NULL no XPtr
 }
 
+const std::vector<std::string> compVec = {"<", ">", "<=", ">=", "==",
+                                          ">,<", ">=,<", ">,<=", ">=,<="};
+enum myComps {
+    less = 0,
+    greater = 1,
+    lessEql = 2,
+    greaterEql = 3,
+    eql = 4,
+    greaterLess = 5,
+    greaterEqlLess = 6,
+    greaterLessEql = 7,
+    greaterEqlLessEql = 8
+};
+
 Rcpp::XPtr<compPtr> putCompPtrInXPtr(std::string fstr) {
-    if (fstr == "<")
-        return(Rcpp::XPtr<compPtr>(new compPtr(&lessCpp)));
-    else if (fstr == ">")
-        return(Rcpp::XPtr<compPtr>(new compPtr(&greaterCpp)));
-    else if (fstr == "<=")
-        return(Rcpp::XPtr<compPtr>(new compPtr(&lessEqualCpp)));
-    else if (fstr == ">=")
-        return(Rcpp::XPtr<compPtr>(new compPtr(&greaterEqualCpp)));
-    else if (fstr == "==")
-        return(Rcpp::XPtr<compPtr>(new compPtr(&equalCpp)));
-    else
-        return Rcpp::XPtr<compPtr>(R_NilValue); // runtime error as NULL no XPtr
+    
+    std::vector<std::string>::const_iterator it = std::find(compVec.begin(), 
+                                                            compVec.end(), fstr);
+    int myIndex = std::distance(compVec.begin(), it);
+    
+    switch(myIndex) {
+        case less:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&lessCpp)));
+        case greater:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterCpp)));
+        case lessEql:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&lessEqualCpp)));
+        case greaterEql:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterEqualCpp)));
+        case eql:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&equalCpp)));
+        case greaterLess:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterLessCpp)));
+        case greaterEqlLess:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterEqlLessCpp)));
+        case greaterLessEql:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterLessEqlCpp)));
+        case greaterEqlLessEql:
+            return(Rcpp::XPtr<compPtr>(new compPtr(&greaterEqlLessEqlCpp)));
+        default:
+            return Rcpp::XPtr<compPtr>(R_NilValue); // runtime error as NULL no XPtr
+    }
 }
