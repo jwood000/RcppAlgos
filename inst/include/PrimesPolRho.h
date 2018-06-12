00baf70f7d026eed4a43468baea417e37e8322d1
Joseph Brant Wood <jwood000@gmail.com>
2018-06-12 15:42:45 -0400

    Declared arrays as const

--- inst/include/PrimesPolRho.h
+++ inst/include/PrimesPolRho.h
@@ -4,7 +4,7 @@
  // next prime number (e.g. p = 2 + primesDiffPR[0] = 3, 
  // p = p + primesDiffPR[1] = 5, etc.)
 
-static int primesDiffPR[549] = {
+static const int primesDiffPR[549] = {
     1,2,2,4,2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,8,4,2,4,
     2,4,14,4,6,2,10,2,6,6,4,6,6,2,10,2,4,2,12,12,4,2,4,6,
     2,10,6,6,6,2,6,4,2,10,14,4,2,4,14,6,10,2,4,6,8,6,6,4,
