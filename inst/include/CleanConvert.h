dbcc521ec094024429ad0cf2f352ff0f3842ea27
Joseph Brant Wood <jwood000@gmail.com>
2018-08-28 05:08:40 -0400

    Added converter funtions for SEXP objects

---
+++ inst/include/CleanConvert.h
@@ -0,0 +1,83 @@
+#ifndef CLEAN_CONVERT_H
+#define CLEAN_CONVERT_H
+
+#include <importExportMPZ.h>
+
+namespace CleanConvert {
+
+    template <typename stdType>
+    void convertPrimitive(SEXP input, stdType &result,
+                          std::string myErrorMsg) {
+        
+        switch(TYPEOF(input)) {
+            case REALSXP: {
+                result = Rcpp::as<stdType>(input);
+                break;
+            }
+            case INTSXP: {
+                result = Rcpp::as<stdType>(input);
+                break;
+            }
+            case RAWSXP:
+            case STRSXP: {
+                mpz_t temp[1];
+                mpz_init(temp[0]);
+                createMPZArray(input, temp, 1);
+                double dblTemp = mpz_get_d(temp[0]);
+                result = dblTemp;
+                mpz_clear(temp[0]);
+                break;
+            }
+            default:
+                Rcpp::stop(myErrorMsg);
+        }
+    }
+    
+    template <typename stdType>
+    void convertVector(SEXP input, std::vector<stdType> &result,
+                       std::string myErrorMsg) {
+        
+        int total = Rf_length(input);
+        
+        switch(TYPEOF(input)) {
+            case REALSXP: {
+                result = Rcpp::as<std::vector<stdType> >(input);
+                break;
+            }
+            case INTSXP: {
+                result = Rcpp::as<std::vector<stdType> >(input);
+                break;
+            }
+            case RAWSXP: {
+                const char* raw = (char*)RAW(input);
+                total = ((int*)raw)[0];
+                // do not put a break here. Fall to
+                // the next case for complete conversion
+            }
+            case STRSXP: {
+                mpz_t *temp;
+                temp = (mpz_t *) malloc(sizeof(mpz_t) * total);
+                
+                for (int i = 0; i < total; ++i)
+                    mpz_init(temp[i]);
+                
+                createMPZArray(input, temp, total);
+                std::vector<double> dblTemp(total);
+                
+                for (int i = 0; i < total; ++i) {
+                    dblTemp[i] = mpz_get_d(temp[i]);
+                    result.push_back((stdType) dblTemp[i]);
+                }
+                
+                for (int i = 0; i < total; ++i)
+                    mpz_clear(temp[i]);
+                
+                break;
+            }
+            default:
+                Rcpp::stop(myErrorMsg);
+        }
+    }
+}
+
+#endif
