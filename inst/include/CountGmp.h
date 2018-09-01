bdeaed316f034f81a1a9b759b878e64c82cd4b95
Joseph Brant Wood <jwood000@gmail.com>
2018-09-01 16:44:08 -0400

    Changed to pass by reference

--- inst/include/CountGmp.h
+++ inst/include/CountGmp.h
@@ -4,11 +4,11 @@
 #include <Rcpp.h>
 #include <gmp.h>
 
-void NumPermsWithRepGmp(mpz_t result, std::vector<int> v);
+void NumPermsWithRepGmp(mpz_t result, std::vector<int> &v);
 void NumPermsNoRepGmp(mpz_t result, int n, int k);
 void nChooseKGmp(mpz_t result, int n, int k);
 void NumCombsWithRepGmp(mpz_t result, int n, int r);
-void MultisetCombRowNumGmp(mpz_t result, int n, int r, std::vector<int> Reps);
-void MultisetPermRowNumGmp(mpz_t result, int n, int r, std::vector<int> myReps);
+void MultisetCombRowNumGmp(mpz_t result, int n, int r, std::vector<int> &Reps);
+void MultisetPermRowNumGmp(mpz_t result, int n, int r, std::vector<int> &myReps);
 
 #endif
