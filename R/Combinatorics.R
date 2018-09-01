cd8dc70d7ce8a59fc2dae9f7ec699507d4e4b4dd
Joseph Brant Wood <jwood000@gmail.com>
2018-09-01 16:39:48 -0400

    Changed sampling input checks. Added FUN arg.

--- R/Combinatorics.R
+++ R/Combinatorics.R
@@ -1,88 +1,56 @@
 comboGeneral <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
                          lower = NULL, upper = NULL, constraintFun = NULL,
                          comparisonFun = NULL, limitConstraints = NULL, 
-                         keepResults = FALSE) {
+                         keepResults = FALSE, FUN = NULL) {
     
     isFactor <- is.factor(v)
-    CombinatoricsRcpp(v, m, repetition, freqs, lower,
-                      upper, constraintFun, 
-                      comparisonFun, limitConstraints,
-                      TRUE, keepResults, isFactor, FALSE)
+    CombinatoricsRcpp(v, m, repetition, freqs, lower, upper, constraintFun, 
+                      comparisonFun, limitConstraints, TRUE, keepResults,
+                      isFactor, FALSE, FUN, new.env())
 }
 
 permuteGeneral <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
                            lower = NULL, upper = NULL, constraintFun = NULL,
                            comparisonFun = NULL, limitConstraints = NULL,
-                           keepResults = FALSE) {
+                           keepResults = FALSE, FUN = NULL) {
     
     isFactor <- is.factor(v)
-    CombinatoricsRcpp(v, m, repetition, freqs, lower,
-                      upper, constraintFun, 
-                      comparisonFun, limitConstraints,
-                      FALSE, keepResults, isFactor, FALSE)
+    CombinatoricsRcpp(v, m, repetition, freqs, lower, upper, constraintFun, 
+                      comparisonFun, limitConstraints, FALSE, keepResults,
+                      isFactor, FALSE, FUN, new.env())
 }
 
 comboCount <-  function(v, m = NULL, repetition = FALSE, freqs = NULL) {
     
     CombinatoricsRcpp(v, m, repetition, freqs, NULL,
-                      NULL, NULL, NULL, NULL,
-                      TRUE, FALSE, FALSE, TRUE)
+                      NULL, NULL, NULL, NULL, TRUE, 
+                      FALSE, FALSE, TRUE, NULL, NULL)
 }
 
 permuteCount <- function(v, m = NULL, repetition = FALSE, freqs = NULL) {
     
     CombinatoricsRcpp(v, m, repetition, freqs, NULL,
-                      NULL, NULL, NULL, NULL,
-                      FALSE, FALSE, FALSE, TRUE)
+                      NULL, NULL, NULL, NULL, FALSE,
+                      FALSE, FALSE, TRUE, NULL, NULL)
 }
 
-comboSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
-                         n = NULL, sampleVec = NULL) {
-    
-    myCount <- comboCount(v, m, repetition, freqs)
-    
-    if (is.null(sampleVec)) {
-        if (is.null(n)){
-            stop("n and sampleVec cannot both be NULL")
-        } else {
-            if (!is.numeric(n))
-                stop("n must be a number")
-            else if (length(n) > 1)
-                stop("length of n must be 1. For specific combinations, use sampleVec.")
-            else if (n > myCount)
-                stop("n exceeds the maximum number of possible results")
-        } 
-        sampleVec = sample(myCount, n)
-    }
+comboSample <- function(v, m = NULL, repetition = FALSE,
+                        freqs = NULL, n = NULL, sampleVec = NULL, 
+                        seed = NULL, FUN = NULL) {
     
     isFactor <- is.factor(v)
-    n <- length(sampleVec)
-    
-    SampleRcpp(v, m, repetition, freqs, sampleVec, TRUE, isFactor, myCount)
+    if (!is.null(seed)) {set.seed(seed)}
+    SampleRcpp(v, m, repetition, freqs, sampleVec, TRUE,
+               isFactor, seed, n, sample, FUN, new.env())
 }
 
-permuteSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
-                          n = NULL, sampleVec = NULL) {
-    
-    myCount <- permuteCount(v, m, repetition, freqs)
-    
-    if (is.null(sampleVec)) {
-        if (is.null(n)){
-            stop("n and sampleVec cannot both be NULL")
-        } else {
-            if (!is.numeric(n))
-                stop("n must be a number")
-            else if (length(n) > 1)
-                stop("length of n must be 1. For specific permutation, use sampleVec.")
-            else if (n > myCount)
-                stop("n exceeds the maximum number of possible results")
-        } 
-        sampleVec = sample(myCount, n)
-    }
+permuteSample <- function(v, m = NULL, repetition = FALSE, 
+                          freqs = NULL, n = NULL, sampleVec = NULL, 
+                          seed = NULL, FUN = NULL) {
     
     isFactor <- is.factor(v)
-    n <- length(sampleVec)
-    
-    SampleRcpp(v, m, repetition, freqs, sampleVec, FALSE, isFactor, myCount)
+    if (!is.null(seed)) {set.seed(seed)}
+    SampleRcpp(v, m, repetition, freqs, sampleVec, FALSE,
+               isFactor, seed, n, sample, FUN, new.env())
 }
 
