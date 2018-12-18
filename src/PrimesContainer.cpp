509fa4675058ef80fcfebd11968df897f7a47be8
Joseph Brant Wood <jwood000@gmail.com>
2018-12-18 18:37:42 -0500

    Updated constants and added primesieve glue

--- src/PrimesContainer.cpp
+++ src/PrimesContainer.cpp
@@ -9,11 +9,11 @@ namespace PrimeCounting {
     // This is the largest multiple of 2*3*5*7 = 210
     // that is less than 2^15 = 32768 = 32KB. This
     // is the typical size of most CPU's L1 cache
-    constexpr int_fast64_t AlmostL1Cache = 32760;
+    constexpr int_fast64_t Almost210L1Cache = 32760;
 
     constexpr unsigned long int SZ_WHEEL210 = 48;
     constexpr unsigned long int NUM210 = 210;
-    constexpr std::size_t N_WHEELS_PER_SEG = static_cast<std::size_t>(AlmostL1Cache / NUM210);
+    constexpr std::size_t N_WHEELS210_PER_SEG = static_cast<std::size_t>(Almost210L1Cache / NUM210);
     
     static const int_fast64_t ARR_WHEEL210[SZ_WHEEL210] = {
         10, 2, 4, 2, 4, 6, 2, 6, 4, 2, 4, 6, 6, 2, 6, 4, 2, 6, 4, 6, 8, 4, 2, 4,
@@ -25,8 +25,8 @@ namespace PrimeCounting {
     // primes instead of generating them.
     int64_t PiPrime (int64_t maxNum) {
         
-        int_fast64_t segSize = AlmostL1Cache;
-        unsigned long int numSegs = N_WHEELS_PER_SEG;
+        int_fast64_t segSize = Almost210L1Cache;
+        std::size_t nWheels = N_WHEELS210_PER_SEG;
         std::size_t szWheel210 = SZ_WHEEL210;
         int sqrtBound = static_cast<int>(std::sqrt(maxNum));
         
@@ -67,7 +67,7 @@ namespace PrimeCounting {
                 nextStrt[i] = j - segSize;
             }
             
-            for (std::size_t q = 0; q < numSegs; ++q)
+            for (std::size_t q = 0; q < nWheels; ++q)
                 for (std::size_t w = 0; w < szWheel210; myNum += ARR_WHEEL210[w], ++w)
                     if (sieve[myNum - lowerBnd])
                         ++count;
@@ -89,7 +89,7 @@ namespace PrimeCounting {
                 nextStrt[i] = j - segSize;
             }
             
-            for (std::size_t q = 0; q < numSegs && myNum <= maxNum; ++q)
+            for (std::size_t q = 0; q < nWheels && myNum <= maxNum; ++q)
                 for (std::size_t w = 0; w < szWheel210 && myNum <= maxNum; myNum += ARR_WHEEL210[w], ++w)
                     if (sieve[myNum - lowerBnd])
                         ++count;
@@ -570,3 +570,96 @@ SEXP MotleyPrimes(SEXP Rb1, SEXP Rb2, SEXP RIsList,
         return EulerPhiSieveCpp<Rcpp::IntegerVector>((int32_t) myMin, (int32_t) myMax, isNamed);
     }
 }
+
+// [[Rcpp::export]]
+SEXP EratosthenesRcpp(SEXP Rb1, SEXP Rb2, SEXP RNumThreads, int maxCores, int maxThreads) {
+    
+    double bound1, bound2;
+    int_fast64_t myMax, myMin;
+    CleanConvert::convertPrimitive(Rb1, bound1, "bound1 must be of type numeric or integer", false);
+    
+    if (bound1 <= 0 || bound1 > PrimeSieve::Significand53)
+        Rcpp::stop("bound1 must be a positive number less than 2^53");
+    
+    if (Rf_isNull(Rb2)) {
+        bound2 = 1;
+    } else {
+        CleanConvert::convertPrimitive(Rb2, bound2, "bound2 must be of type numeric or integer", false);
+    }
+    
+    if (bound2 <= 0 || bound2 > PrimeSieve::Significand53)
+        Rcpp::stop("bound2 must be a positive number less than 2^53");
+    
+    if (bound1 > bound2) {
+        myMax = static_cast<int_fast64_t>(std::floor(bound1));
+        myMin = static_cast<int_fast64_t>(std::ceil(bound2));
+    } else {
+        myMax = static_cast<int_fast64_t>(std::floor(bound2));
+        myMin = static_cast<int_fast64_t>(std::ceil(bound1));
+    }
+    
+    if (myMax <= 1)
+        return Rcpp::IntegerVector();
+    
+    if (myMin <= 2) myMin = 1;
+    if (myMin == myMax) {++myMax;}
+    
+    int nThreads = 1;
+    if (!Rf_isNull(RNumThreads))
+        CleanConvert::convertPrimitive(RNumThreads, nThreads, "nThreads must be of type numeric or integer");
+    
+    std::size_t numPrimes = 0u;
+    std::vector<unsigned long int> runningCount;
+    runningCount.push_back(0u);
+    unsigned long int numSects = nThreads;
+    bool Parallel = false;
+    
+    if (myMax > INT_MAX) {
+        std::vector<std::vector<double>> primeList(numSects, std::vector<double>());
+        std::vector<double> tempPrime;
+        
+        PrimeSieve::PrimeMaster(myMin, myMax, tempPrime, primeList, 
+                                Parallel, nThreads, maxThreads, maxCores);
+        
+        if (Parallel) {
+            for (int i = 0; i < numSects; ++i) {
+                numPrimes += primeList[i].size();
+                runningCount.push_back(numPrimes);
+            }
+            
+            Rcpp::NumericVector primes(numPrimes);
+            Rcpp::NumericVector::iterator priBeg = primes.begin();
+            
+            for (int i = 0; i < numSects; ++i)
+                std::copy(primeList[i].begin(), primeList[i].end(), priBeg + runningCount[i]);
+            
+            return primes;
+        } else {
+            return Rcpp::wrap(tempPrime);
+        }
+    } else {
+        std::vector<std::vector<int_fast32_t>> primeList(numSects, std::vector<int_fast32_t>());
+        std::vector<int_fast32_t> tempPrime;
+        
+        PrimeSieve::PrimeMaster(myMin, myMax, tempPrime, primeList, 
+                                Parallel, nThreads, maxThreads, maxCores);
+        
+        if (Parallel) {
+            for (int i = 0; i < numSects; ++i) {
+                numPrimes += primeList[i].size();
+                runningCount.push_back(numPrimes);
+            }
+            
+            Rcpp::IntegerVector primes(numPrimes);
+            Rcpp::IntegerVector::iterator priBeg = primes.begin();
+            
+            for (int i = 0; i < numSects; ++i)
+                std::copy(primeList[i].begin(), primeList[i].end(), priBeg + runningCount[i]);
+            
+            return primes;
+        } else {
+            return Rcpp::wrap(tempPrime);
+        }
+    }
+}
+
