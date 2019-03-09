d1750fbd4de8362f7f98a7c7418ee7aa038ee144
Joseph Brant Wood <jwood000@gmail.com>
2019-03-09 17:06:07 -0500

    Added logical convert function

--- inst/include/CleanConvert.h
+++ inst/include/CleanConvert.h
@@ -6,16 +6,36 @@
 const double Significand53 = 9007199254740991.0;
 
 namespace CleanConvert {
+    
+    inline bool convertLogical(SEXP boolInput, std::string nameOfBool) {
+        bool result = false;
+        
+        if (!Rf_isNull(boolInput)) {
+            if (TYPEOF(boolInput) == LGLSXP) {
+                double dblInp = Rcpp::as<double>(boolInput);
+                int intTest = static_cast<int>(dblInp);
+                
+                if (dblInp != intTest)
+                    Rcpp::stop("NA values are not allowed for " + nameOfBool);
+                
+                result = Rcpp::as<bool>(boolInput);
+            } else {
+                Rcpp::stop("Only logical values are supported for " + nameOfBool);
+            }
+        }
+        
+        return result;
+    }
 
     template <typename stdType>
-    void convertPrimitive(SEXP input, stdType &result, std::string nameOfObject,
+    inline void convertPrimitive(SEXP input, stdType &result, std::string nameOfObject,
                           bool strPoss = false, bool checkWhole = true) {
         
         switch(TYPEOF(input)) {
             case REALSXP:
             case INTSXP: {
                 double dblInp = Rcpp::as<double>(input);
-                int intTest = static_cast<int>(dblInp);
+                int64_t intTest = static_cast<int64_t>(dblInp);
                 if (checkWhole && intTest != dblInp)
                     Rcpp::stop(nameOfObject + " must be a whole number");
                 
@@ -36,12 +56,12 @@ namespace CleanConvert {
                 break;
             }
             default:
-                Rcpp::stop(nameOfObject + " must be convertible to a real number");
+                Rcpp::stop("This type is not supported! No conversion possible for " + nameOfObject);
         }
     }
     
     template <typename stdType>
-    void convertVector(SEXP input, std::vector<stdType> &result, std::string nameOfObject,
+    inline void convertVector(SEXP input, std::vector<stdType> &result, std::string nameOfObject,
                        bool checkWhole = true, bool numOnly = true) {
         
         int total = Rf_length(input);
@@ -54,7 +74,7 @@ namespace CleanConvert {
                     result.resize(vecCheck.size());
                     
                     for (std::size_t i = 0; i < vecCheck.size(); ++i) {
-                        if (static_cast<int>(vecCheck[i]) != vecCheck[i])
+                        if (static_cast<int64_t>(vecCheck[i]) != vecCheck[i])
                             Rcpp::stop("Each element in " + nameOfObject + " must be a whole number");
                         
                         result[i] = static_cast<stdType>(vecCheck[i]);
@@ -99,7 +119,7 @@ namespace CleanConvert {
                 break;
             }
             default:
-                Rcpp::stop(nameOfObject + " must be convertible to real numbers");
+                Rcpp::stop("This type is not supported! No conversion possible for " + nameOfObject);
         }
     }
 }
