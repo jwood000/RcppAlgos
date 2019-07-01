39816a02f06b7dc2ac5b763cfbe7ff75b5d413ee
Joseph Brant Wood <jwood000@gmail.com>
2019-07-01 07:49:52 -0400

    Fixed spacing for testPrimeSieve

--- tests/testthat/testPrimeSieve.R
+++ tests/testthat/testPrimeSieve.R
@@ -17,20 +17,16 @@ test_that("primeSieve generates correct numbers", {
     expect_equal(primeSieve(1, 1), integer(0))
     
     expect_equal(primeSieve(31625, 31630), 31627)
-    
     expect_equal(2L + length(primeSieve(5, 1e5)), length(primeSieve(1e5)))
     
     ## Primes obtained from https://primes.utm.edu
-    expect_equal(primeSieve(1e8, 1e8 + 100), c(100000007, 100000037, 100000039, 
-                                               100000049, 100000073, 100000081))
+    expect_equal(primeSieve(1e8, 1e8 + 100), c(100000007, 100000037, 100000039, 100000049, 100000073, 100000081))
     
     ## Primes obtained from http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
-    expect_equal(primeSieve(1e10, 1e10 + 100), c(10000000019, 10000000033, 
-                                                 10000000061, 10000000069, 10000000097))
+    expect_equal(primeSieve(1e10, 1e10 + 100), c(10000000019, 10000000033, 10000000061, 10000000069, 10000000097))
     
     ## Primes obtained from http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
-    expect_equal(primeSieve(1e12 - 100, 1e12), c(999999999937, 999999999959, 
-                                                 999999999961, 999999999989))
+    expect_equal(primeSieve(1e12 - 100, 1e12), c(999999999937, 999999999959, 999999999961, 999999999989))
     
     ## Large prime gap greater than 1e15 http://www.trnicely.net/gaps/gaps3.html
     expect_equal(primeSieve(1693182318746371, 1693182318746371 + 1132), 
