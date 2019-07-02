9b6ba8884630e0043cb7f03d002a68d101f406cf
Joseph Brant Wood <jwood000@gmail.com>
2019-07-01 23:28:20 -0400

    Uncommented error tests

--- tests/testthat/testPrimeSieve.R
+++ tests/testthat/testPrimeSieve.R
@@ -51,12 +51,12 @@ test_that("primeSieve generates correct numbers", {
     ## system.time(a <- primeSieve(6, 1.1e9, nThreads = 1))
 })
 
-# test_that("primeSieve produces appropriate error messages", {
-#     expect_error(primeSieve(-1), "must be a positive")
-#     expect_error(primeSieve(1,-1), "must be a positive whole number")
-#     expect_error(primeSieve(1,2^53), "must be less than")
-#     expect_error(primeSieve(2^53), "must be less than")
-#     expect_error(primeSieve(2^53, 1), "must be less than")
-#     expect_error(primeSieve(2^4, "1"), "must be of type numeric or integer")
-#     expect_error(primeSieve("500"), "must be of type numeric or integer")
-# })
+test_that("primeSieve produces appropriate error messages", {
+    expect_error(primeSieve(-1), "must be a positive")
+    expect_error(primeSieve(1,-1), "must be a positive whole number")
+    expect_error(primeSieve(1,2^53), "must be less than")
+    expect_error(primeSieve(2^53), "must be less than")
+    expect_error(primeSieve(2^53, 1), "must be less than")
+    expect_error(primeSieve(2^4, "1"), "must be of type numeric or integer")
+    expect_error(primeSieve("500"), "must be of type numeric or integer")
+})
