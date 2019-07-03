4df1379292f768d8115908923d46debe28caa2a8
Joseph Brant Wood <jwood000@gmail.com>
2019-07-02 21:47:21 -0400

    Commented out lengthy tests

--- tests/testthat/testPrimeSieve.R
+++ tests/testthat/testPrimeSieve.R
@@ -43,12 +43,15 @@ test_that("primeSieve generates correct numbers", {
     expect_true(funTestPar(1e12, 1e12 + 1e7))
     expect_true(funTestPar(1e15, 1e15 + 5e8, nT = 4))
     
-    gc()
-    expect_equal(length(primeSieve(633318687598976, 633318707598976)), 587185)
-    gc()
-    expect_equal(length(primeSieve(12e8)), 60454705)
-    gc()
-    expect_equal(length(primeSieve(39582118599936, 39582718599936, nThreads = 4)), 19161558)
+    # The following test can be used for fuller coverage but are commented
+    # because they take a bit of time. All results were confirmed with
+    # primesieve by Kim Walisch
+    # gc()
+    # expect_equal(length(primeSieve(633318687598976, 633318707598976)), 587185)
+    # gc()
+    # expect_equal(length(primeSieve(12e8)), 60454705)
+    # gc()
+    # expect_equal(length(primeSieve(39582118599936, 39582718599936, nThreads = 4)), 19161558)
 })
 
 test_that("primeSieve produces appropriate error messages", {
