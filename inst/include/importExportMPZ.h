d3545835f462b2d28bc3872d292d75cd508a188d
Joseph Brant Wood <jwood000@gmail.com>
2019-09-26 21:18:54 -0400

    Removed space

--- inst/include/importExportMPZ.h
+++ inst/include/importExportMPZ.h
@@ -6,9 +6,9 @@
 
 // Functions for importing/exporting and converting SEXPs to mpz_t
 
-void createMPZArray (SEXP input, mpz_t *myVec, std::size_t sizevec, 
+void createMPZArray(SEXP input, mpz_t *myVec, std::size_t sizevec, 
                      const std::string &nameOfObject, bool negPoss = false);
 
-int myRaw (char* raw, mpz_t value, std::size_t totals);
+int myRaw(char* raw, mpz_t value, std::size_t totals);
 
 #endif
