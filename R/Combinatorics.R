ce975966fbced51dfae7f66e3c8c1eb5c741d828
Joseph Brant Wood <jwood000@gmail.com>
2019-10-10 20:33:04 -0400

    cleanVec experiment

--- R/Combinatorics.R
+++ R/Combinatorics.R
@@ -1,44 +1,99 @@
+purifyVector <- function(v, freqs) {
+    
+    vIsNA <- is.na(v)
+    
+    if (!is.null(freqs) && length(v) != length(freqs)) {
+        stop("the length of freqs must equal the length of v")
+    }
+    
+    if (any(vIsNA)) {
+        if (!is.null(freqs)) freqs <- freqs[-vIsNA]
+        v <- v[-vIsNA]
+    }
+    
+    vOut <- sort(unique(v))
+    
+    if (length(vOut) < length(v)) {
+        vReps <- tabulate(match(v, vOut))
+        
+        if (is.null(freqs)) {
+            return(list(vOut, vReps))
+        } else {
+            freqs <- vapply(split(freqs, v), sum, 1, USE.NAMES = F)
+            return(list(vOut, freqs))
+        }
+    }
+    
+    return(list(vOut, NULL))
+}
+
 comboGeneral <- function(v, m = NULL, repetition = FALSE, freqs = NULL, 
                          lower = NULL, upper = NULL, constraintFun = NULL,
                          comparisonFun = NULL, limitConstraints = NULL, 
                          keepResults = NULL, FUN = NULL, Parallel = FALSE,
-                         nThreads = NULL, tolerance = NULL) {
+                         nThreads = NULL, tolerance = NULL, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     isFactor <- is.factor(v)
     CombinatoricsRcpp(v, m, repetition, freqs, lower, upper, constraintFun, 
-                      comparisonFun, limitConstraints, TRUE, keepResults, isFactor,
-                      FALSE, FUN, new.env(), Parallel, nThreads, pkgEnv$nThreads, tolerance)
+                      comparisonFun, limitConstraints, TRUE, keepResults, isFactor, FALSE,
+                      FUN, new.env(), Parallel, nThreads, pkgEnv$nThreads, tolerance, cleanVec)
 }
 
 permuteGeneral <- function(v, m = NULL, repetition = FALSE, freqs = NULL, 
                            lower = NULL, upper = NULL, constraintFun = NULL,
                            comparisonFun = NULL, limitConstraints = NULL,
                            keepResults = NULL, FUN = NULL, Parallel = FALSE,
-                           nThreads = NULL, tolerance = NULL) {
+                           nThreads = NULL, tolerance = NULL, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     isFactor <- is.factor(v)
     CombinatoricsRcpp(v, m, repetition, freqs, lower, upper, constraintFun, 
-                      comparisonFun, limitConstraints, FALSE, keepResults, isFactor,
-                      FALSE, FUN, new.env(), Parallel, nThreads, pkgEnv$nThreads, tolerance)
+                      comparisonFun, limitConstraints, FALSE, keepResults, isFactor, FALSE,
+                      FUN, new.env(), Parallel, nThreads, pkgEnv$nThreads, tolerance, cleanVec)
 }
 
-comboCount <-  function(v, m = NULL, repetition = FALSE, freqs = NULL) {
+comboCount <-  function(v, m = NULL, repetition = FALSE, freqs = NULL, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     CombinatoricsRcpp(v, m, repetition, freqs, NULL, NULL,
                       NULL, NULL, NULL, TRUE, FALSE, FALSE,
                       TRUE, NULL, NULL, FALSE, NULL, 0, NULL)
 }
 
-permuteCount <- function(v, m = NULL, repetition = FALSE, freqs = NULL) {
+permuteCount <- function(v, m = NULL, repetition = FALSE, freqs = NULL, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     CombinatoricsRcpp(v, m, repetition, freqs, NULL, NULL,
                       NULL, NULL, NULL, FALSE, FALSE, FALSE,
-                      TRUE, NULL, NULL, FALSE, NULL, 0, NULL)
+                      TRUE, NULL, NULL, FALSE, NULL, 0, NULL, cleanVec)
 }
 
 comboSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
-                        n = NULL, sampleVec = NULL, seed = NULL, FUN = NULL,
-                        Parallel = FALSE, nThreads = NULL, namedSample = FALSE) {
+                        n = NULL, sampleVec = NULL, seed = NULL, 
+                        FUN = NULL, Parallel = FALSE, nThreads = NULL, 
+                        namedSample = FALSE, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     isFactor <- is.factor(v)
     if (!is.null(seed)) {set.seed(seed)}
@@ -48,8 +103,14 @@ comboSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
 }
 
 permuteSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
-                          n = NULL, sampleVec = NULL, seed = NULL, FUN = NULL,
-                          Parallel = FALSE, nThreads = NULL, namedSample = FALSE) {
+                          n = NULL, sampleVec = NULL, seed = NULL, 
+                          FUN = NULL, Parallel = FALSE, nThreads = NULL, 
+                          namedSample = FALSE, cleanVec = FALSE) {
+    
+    if (cleanVec) {
+        myL <- purifyVector(v, freqs)
+        v <- myL[[1]]; freqs <- myL[[2]]
+    }
     
     isFactor <- is.factor(v)
     if (!is.null(seed)) {set.seed(seed)}
