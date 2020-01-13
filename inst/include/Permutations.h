d12fcaa67741fcd5c2df340e3ff39abfee0cb7c8
Joseph Brant Wood <jwood000@gmail.com>
2020-01-13 16:42:02 -0500

    Removed FunApply functions

--- inst/include/Permutations.h
+++ inst/include/Permutations.h
@@ -2,8 +2,10 @@
 #define PERMUTATIONS_H
 
 #include "NextStandard.h"
-#include "CombPermUtils.h"
+#include "StandardCount.h"
+#include "Cpp14MakeUnique.h"
 #include <RcppThread.h>
+#include <cmath>
 
 constexpr int unrollSize = 8;
 
@@ -318,70 +320,6 @@ void MultisetPermutation(typeMatrix &matRcpp, const typeVector &v, std::vector<i
     // Get last permutation
     for (int j = 0; j < m; ++j)
         matRcpp(numR1, j) = v[arrPerm[j]];
-}
-
-template <typename typeVector>
-void PermutationApplyFun(Rcpp::List &myList, const typeVector &v, std::vector<int> z, int n,
-                         int m, bool IsRep, bool IsMult, int nRows, SEXP sexpFun, SEXP rho) {
-    
-    const int lenFreqs = (IsMult) ? z.size() : 0;
-    typeVector vectorPass(m);
-    
-    const int numR1 = nRows - 1;
-    const int lastCol = m - 1;
-    const int maxInd = (IsMult) ? (lenFreqs - 1) : (n - 1);
-    
-    if (IsRep) {
-        for (int count = 0; count < nRows; ++count) {
-            for (int j = 0; j < m; ++j)
-                vectorPass[j] = v[z[j]];
-
-            SETCADR(sexpFun, vectorPass);
-            myList[count] = Rf_eval(sexpFun, rho);
-
-            for (int i = lastCol; i >= 0; --i) {
-                if (z[i] != maxInd) {
-                    ++z[i];
-                    break;
-                } else {
-                    z[i] = 0;
-                }
-            }
-        }
-    } else {
-        const int arrLength = maxInd + 1;
-        auto arrPerm = FromCpp14::make_unique<int[]>(arrLength);
-        
-        for (int i = 0; i < arrLength; ++i)
-            arrPerm[i] = z[i];
-        
-        if (m == n || m == lenFreqs) {
-            for (int count = 0; count < numR1; ++count) {
-                for (int j = 0; j < m; ++j)
-                    vectorPass[j] = v[arrPerm[j]];
-                
-                SETCADR(sexpFun, vectorPass);
-                myList[count] = Rf_eval(sexpFun, rho);
-                nextFullPerm(arrPerm.get(), maxInd);
-            }
-        } else {
-            for (int count = 0; count < numR1; ++count) {
-                for (int j = 0; j < m; ++j)
-                    vectorPass[j] = v[arrPerm[j]];
-                    
-                SETCADR(sexpFun, vectorPass);
-                myList[count] = Rf_eval(sexpFun, rho);
-                nextPartialPerm(arrPerm.get(), lastCol, maxInd);
-            }
-        }
-        
-        // Get last permutation
-        for (int j = 0; j < m; ++j)
-            vectorPass[j] = v[arrPerm[j]];
-
-        SETCADR(sexpFun, vectorPass);
-        myList[numR1] = Rf_eval(sexpFun, rho);
-    }
 }
 
 #endif
