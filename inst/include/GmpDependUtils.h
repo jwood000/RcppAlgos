a82d040bcee2d280e1b91c815491001069a9b055
Joseph Brant Wood <jwood000@gmail.com>
2020-04-17 08:21:04 -0400

    Simplified string conversion

--- inst/include/GmpDependUtils.h
+++ inst/include/GmpDependUtils.h
@@ -47,8 +47,7 @@ inline void SetSampleNames(bool IsGmp, std::size_t sampSize, typeRcpp &objRcpp,
             mpz_add_ui(myBigSamp[i], myBigSamp[i], 1);
             auto buffer = FromCpp14::make_unique<char[]>(mpz_sizeinbase(myBigSamp[i], base10) + 2);
             mpz_get_str(buffer.get(), base10, myBigSamp[i]);
-            const std::string tempRowName = buffer.get();
-            myRowNames[i] = tempRowName;
+            myRowNames[i] = Rf_mkChar(buffer.get());
         }
     } else {
         for (std::size_t i = 0; i < sampSize; ++i)
