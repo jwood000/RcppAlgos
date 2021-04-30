3b8078e11d500ba628a79e195c46aa6c1dc0f742
Joseph Brant Wood <jwood000@gmail.com>
2021-04-30 05:50:04 -0400

    Added functionns to namespace

--- R/Partitions.R
+++ R/Partitions.R
@@ -1,25 +1,27 @@
-partitionGeneral <- function(v, m = NULL, repetition = FALSE, freqs = NULL,
-                             lower = NULL, upper = NULL, target = NULL, 
-                             nThreads = NULL, tolerance = NULL) {
+partitionGeneral <- function(v, m = NULL, repetition = FALSE,
+                             freqs = NULL, target, lower = NULL,
+                             upper = NULL, nThreads = NULL,
+                             tolerance = NULL) {
     
     return(.Call(CombinatoricsCnstrt, v, m, repetition,
                  freqs, lower, upper, "sum", "==",
-                 target, TRUE, FALSE, FALSE,
-                 nThreads, pkgEnv$nThreads, tolerance,
+                 target, TRUE, FALSE, FALSE, nThreads,
+                 pkgEnv$nThreads, tolerance,
                  PACKAGE = "RcppAlgos"))
 }
 
-# comboSample <- function(v, m = NULL, repetition = FALSE, freqs = NULL, n = NULL, 
-#                         sampleVec = NULL, seed = NULL, FUN = NULL, Parallel = FALSE,
-#                         nThreads = NULL, namedSample = FALSE) {
-#     
-#     if (!is.null(seed)) {set.seed(seed)}
-#     SampleRcpp(v, m, repetition, freqs, sampleVec, TRUE, is.factor(v), seed, n,
-#                sample, FUN, new.env(), Parallel, nThreads, pkgEnv$nThreads, namedSample)
-# }
+partitionCount <-  function(v, m = NULL, repetition = FALSE,
+                            freqs = NULL, target) {
+    .Call(PartitionsCount, target, v, m, repetition, freqs,
+          "==", NULL, NULL, FALSE, PACKAGE = "RcppAlgos");
+}
 
-partitionCount <-  function(v, m = NULL, repetition = FALSE, freqs = NULL) {
-    .Call(CombinatoricsCount, v, m,
-          repetition, freqs, TRUE,
-          PACKAGE = "RcppAlgos");
+partitionDesign <-  function(v, m = NULL, repetition = FALSE,
+                             freqs = NULL, target) {
+    .Call(PartitionsCount, target, v, m, repetition, freqs,
+          "==", NULL, NULL, TRUE, PACKAGE = "RcppAlgos");
+}
+
+partLen <- function(tar, m, v, rep = FALSE, fr = NULL) {
+    RcppAlgos241::comboGeneral(v, m, rep, fr, constraintFun = "sum", comparisonFun = "==", limitConstraints = tar)
 }
