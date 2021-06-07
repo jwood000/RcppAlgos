08456f068e280bd98ca90a5397f71b781b101da0
Joseph Brant Wood <jwood000@gmail.com>
2021-06-06 22:11:14 -0400

    Added s to be more consistent

--- R/Partitions.R
+++ R/Partitions.R
@@ -3,10 +3,10 @@ GetTarget <- function(v, target) {
     return(target)
 }
 
-partitionGeneral <- function(v, m = NULL, repetition = FALSE,
-                             freqs = NULL, target = NULL, lower = NULL,
-                             upper = NULL, nThreads = NULL,
-                             tolerance = NULL) {
+partitionsGeneral <- function(v, m = NULL, repetition = FALSE,
+                              freqs = NULL, target = NULL, lower = NULL,
+                              upper = NULL, nThreads = NULL,
+                              tolerance = NULL) {
     
     return(.Call(CombinatoricsCnstrt, v, m, repetition,
                  freqs, lower, upper, "sum", "==",
@@ -15,15 +15,15 @@ partitionGeneral <- function(v, m = NULL, repetition = FALSE,
                  PACKAGE = "RcppAlgos"))
 }
 
-partitionCount <- function(v, m = NULL, repetition = FALSE,
-                           freqs = NULL, target = NULL) {
+partitionsCount <- function(v, m = NULL, repetition = FALSE,
+                            freqs = NULL, target = NULL) {
     .Call(PartitionsCount, GetTarget(v, target), v, m, repetition,
           freqs, "==", NULL, NULL, FALSE, FALSE, PACKAGE = "RcppAlgos");
 }
 
-partitionDesign <- function(v, m = NULL, repetition = FALSE,
-                            freqs = NULL, target = NULL,
-                            showDesign = FALSE) {
+partitionsDesign <- function(v, m = NULL, repetition = FALSE,
+                             freqs = NULL, target = NULL,
+                             showDesign = FALSE) {
     
     .Call(PartitionsCount, GetTarget(v, target),
           v, m, repetition, freqs, "==", NULL, NULL,
