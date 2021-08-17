2470d8bfafaa526bb1a4e5007e5a7e808a67630f
Joseph Brant Wood <jwood000@gmail.com>
2021-08-17 18:07:06 -0400

    Updated NAMESPACE with comboGroup et al

--- R/ComboGroups.R
+++ R/ComboGroups.R
@@ -1,21 +1,25 @@
-# comboGroups <- function(v, numGroups, retType = "matrix", lower = NULL,
-#                         upper = NULL, Parallel = FALSE, nThreads = NULL) {
-#     
-#     ComboGroupsRcpp(v, numGroups, retType, lower, upper, is.factor(v),
-#                     Parallel, nThreads, pkgEnv$nThreads, FALSE, NULL, 
-#                     NULL, NULL, sample, FALSE)
-# }
-# 
-# comboGroupsSample <- function(v, numGroups, retType = "matrix", n = NULL,
-#                               sampleVec = NULL, seed = NULL, Parallel = FALSE, 
-#                               nThreads = NULL, namedSample = FALSE) {
-#     
-#     if (!is.null(seed)) {set.seed(seed)}
-#     ComboGroupsRcpp(v, numGroups, retType, NULL, NULL, is.factor(v),
-#                     Parallel, nThreads, pkgEnv$nThreads, TRUE, sampleVec,
-#                     seed, n, sample, namedSample)
-# }
-# 
-# comboGroupsCount <- function(v, numGroups) {
-#     ComboGroupsCountCpp(v, numGroups)
-# }
+comboGroups <- function(v, numGroups, retType = "matrix", lower = NULL,
+                        upper = NULL, Parallel = FALSE, nThreads = NULL) {
+
+    return(.Call(ComboGroupsCpp, v, numGroups, retType, lower,
+                 upper, Parallel, nThreads, pkgEnv$nThreads, FALSE, NULL,
+                 NULL, NULL, sample, FALSE, NULL, PACKAGE = "RcppAlgos"))
+}
+
+comboGroupsSample <- function(v, numGroups, retType = "matrix", n = NULL,
+                              sampleVec = NULL, seed = NULL, Parallel = FALSE,
+                              nThreads = NULL, namedSample = FALSE) {
+
+    if (!is.null(seed)) {
+        set.seed(seed)
+    }
+
+    return(.Call(ComboGroupsCpp, v, numGroups, retType, NULL, NULL,
+                 Parallel, nThreads, pkgEnv$nThreads, TRUE, sampleVec,
+                 seed, n, sample, namedSample, new.env(),
+                 PACKAGE = "RcppAlgos"))
+}
+
+comboGroupsCount <- function(v, numGroups) {
+    return(.Call(ComboGroupsCountCpp, v, numGroups, PACKAGE = "RcppAlgos"))
+}
