c455fc0bc60e7bbbb10323b400920f0bd1accc3d
Joseph Wood <jwood000@gmail.com>
2025-01-20 17:08:00 -0500

    Added command line code to remove reprex additional code blocking pattern

--- scripts/create_md.R
+++ scripts/create_md.R
@@ -5,13 +5,15 @@ my_scripts <- list.files("../scripts", full.names = TRUE)
 my_scripts <- my_scripts[which(!grepl("create_md", my_scripts))]
 
 for (f in my_scripts) {
-    source(f)
-    print(f)
-    file.rename(list.files(pattern = "*_reprex.md"),
-                to = paste0(gsub(".*/(.*).R", "\\1", f), ".md"))
-    sapply(list.files(pattern = ".*.R"), file.remove)
-    sapply(list.files(pattern = ".*.out"), file.remove)
-    print(gsub(".*/(.*).R", "\\1", f))
+    if (!dir.exists(f)) {
+        source(f)
+        print(f)
+        file.rename(list.files(pattern = "*_reprex.md"),
+                    to = paste0(gsub(".*/(.*).R", "\\1", f), ".md"))
+        sapply(list.files(pattern = ".*.R"), file.remove)
+        sapply(list.files(pattern = ".*.out"), file.remove)
+        print(gsub(".*/(.*).R", "\\1", f))
+    }
 }
 
 system("mv *.md ../")
@@ -19,3 +21,20 @@ setwd("../")
 system("rm -r temp_md")
 ## Run on command line... the escapes are killing me!
 ## perl -p -i -e 's/\(http(.*?)\)/\(<http$1>\)/g' *.md
+##
+## This command removes the additional coding blocks:
+##
+## code...
+## ```
+##
+## ``` r
+##
+## more code
+##
+## -- to --
+##
+## code
+##
+## more code
+##
+## perl -0777p -i -e 's/```\n\n``` r\n//g' *.md
