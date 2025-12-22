ddcc9fcc58f5cb678a8632d149265ab0da701036
Joseph Wood <jwood000@gmail.com>
2025-12-22 13:01:44 -0500

    whitespace refactor

--- inst/include/Partitions/PartitionsManager.h
+++ inst/include/Partitions/PartitionsManager.h
@@ -3,8 +3,10 @@
 #include "Partitions/PartitionsTypes.h"
 #include "RMatrix.h"
 
-int PartsStdManager(int* mat, std::vector<int> &z, int width, int lastElem,
-                    int lastCol, int nRows, PartitionType ptype);
+int PartsStdManager(
+    int* mat, std::vector<int> &z, int width, int lastElem,
+    int lastCol, int nRows, PartitionType ptype
+);
 
 int PartsStdParallel(RcppParallel::RMatrix<int> &mat, std::vector<int> &z,
                      int strt, int width, int lastElem, int lastCol,
@@ -12,9 +14,9 @@ int PartsStdParallel(RcppParallel::RMatrix<int> &mat, std::vector<int> &z,
 
 template <typename T>
 int PartsGenParallel(RcppParallel::RMatrix<T> &mat,
-                     const std::vector<T> &v, std::vector<int> &z,
-                     int strt, int width, int lastElem, int lastCol,
-                     int nRows, PartitionType ptype);
+                     const std::vector<T> &v, std::vector<int> &z, int strt,
+                     int width, int lastElem, int lastCol, int nRows,
+                     PartitionType ptype);
 
 template <typename T>
 int PartsGenManager(T* mat, const std::vector<T> &v, std::vector<int> &z,
